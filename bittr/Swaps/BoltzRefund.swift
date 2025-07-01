a45c370e50deee047cf9ae8a6f4958eb9fd4b8bc
Ruben Waterman <rubenwaterman@me.com>
2025-07-01 09:05:16 +0000

    no message

--- bittr/Swaps/BoltzRefund.swift
+++ bittr/Swaps/BoltzRefund.swift
@@ -11,25 +11,25 @@ class BoltzRefund {
     static func tryBoltzRefund() async throws -> Bool {
         // This is the public key we got when we made the API call:
         //        {
-        //           acceptZeroConf = 0;
-        //           address = bcrt1pnvdn47r9lwwdtv0wax3537j5xtvyg4z9jk2qut7e47vxawxx028s305jsh;
-        //           bip21 = "bitcoin:bcrt1pnvdn47r9lwwdtv0wax3537j5xtvyg4z9jk2qut7e47vxawxx028s305jsh?amount=0.00300602&label=Send%20to%20BTC%20lightning";
-        //           claimPublicKey = 034d0ec2790580f2f22b2b6e7e56ca30962ea2395f01cb563afe24440e3117fc55;
-        //           expectedAmount = 300602;
-        //           id = tgTKex31LpzS;
-        //           swapTree =     {
-        //               claimLeaf =         {
-        //                   output = a914227e1df5d85c2eb50e78b2e041bf0c3b8e2f086588204d0ec2790580f2f22b2b6e7e56ca30962ea2395f01cb563afe24440e3117fc55ac;
-        //                   version = 192;
-        //               };
-        //               refundLeaf =         {
-        //                   output = 20ef07ea4cacba6709d43a74278a7b6c792cbcef10a035f04d2f196c9069876876ad02b204b1;
-        //                   version = 192;
-        //               };
-        //           };
-        //           timeoutBlockHeight = 1202;
-        //       }
-        let boltzServerPublicKeyBytes = try! "034d0ec2790580f2f22b2b6e7e56ca30962ea2395f01cb563afe24440e3117fc55".bytes
+        //            acceptZeroConf = 0;
+        //            address = bcrt1p4m0sj58tex70w95y8v25tlqcq7zqn73636r24qyk8s3y249nrd9syvqk2e;
+        //            bip21 = "bitcoin:bcrt1p4m0sj58tex70w95y8v25tlqcq7zqn73636r24qyk8s3y249nrd9syvqk2e?amount=0.00200502&label=Send%20to%20BTC%20lightning";
+        //            claimPublicKey = 03d0ceae7a2076302b7418fd8ad1c8e6f05cb5e8f24116813074e1fb4e87d3b523;
+        //            expectedAmount = 200502;
+        //            id = t6qPA5n7IAIQ;
+        //            swapTree =     {
+        //                claimLeaf =         {
+        //                    output = a91439aa8f251488b1a02fc89fe448f9bbf45a3b01f48820d0ceae7a2076302b7418fd8ad1c8e6f05cb5e8f24116813074e1fb4e87d3b523ac;
+        //                    version = 192;
+        //                };
+        //                refundLeaf =         {
+        //                    output = 20f8b2dfc86aa1f5c6df0d3089c74088eaf0527216b61472113e8839e4e4bbb69fad02c004b1;
+        //                    version = 192;
+        //                };
+        //            };
+        //            timeoutBlockHeight = 1216;
+        //        }
+        let boltzServerPublicKeyBytes = try! "03d0ceae7a2076302b7418fd8ad1c8e6f05cb5e8f24116813074e1fb4e87d3b523".bytes
         
         let boltzServerPublicKey = try! P256K.Schnorr.PublicKey(
             dataRepresentation: boltzServerPublicKeyBytes,
@@ -46,7 +46,7 @@ class BoltzRefund {
         let ourPrivateKey = try! P256K.Schnorr.PrivateKey.init(dataRepresentation: ourPrivateKeyBytes)
         
         // For some reason, when I want to later on use the getSighash function, that only accepts uncompressed keys, so we're using that as the format here already
-        let boltzAggregateKey = try P256K.MuSig.aggregate([boltzServerPublicKey, ourPrivateKey.publicKey], format: P256K.Format.compressed)
+        let boltzAggregateKey = try P256K.MuSig.aggregate([boltzServerPublicKey, ourPrivateKey.publicKey])
         
         // The boltzAggregateKey looks like this: 04499bcea8f3dbf842f347c30b08ae4e3e29141e689c8f1de82c9fd6f37b57d5c4f2712db01543c5d9226b6629294f95958efd2994ae071cd20e5f151e02004a8a
         let hexString = String(bytes: boltzAggregateKey.dataRepresentation)
@@ -54,7 +54,7 @@ class BoltzRefund {
         print("hexString: \(hexString)")
         
         // Now, we take the refundLeaf.output from our Swap and tweak the key, again because we need an uncompressed key later on to create the sigHash, we put format .uncompressed
-        let tweak = try! "20ef07ea4cacba6709d43a74278a7b6c792cbcef10a035f04d2f196c9069876876ad02b204b1".bytes
+        let tweak = try! "20f8b2dfc86aa1f5c6df0d3089c74088eaf0527216b61472113e8839e4e4bbb69fad02c004b1".bytes
         let tweakedKey = try! boltzAggregateKey.add(tweak, format: P256K.Format.compressed)
         
         let hexTweakedString = String(bytes: tweakedKey.dataRepresentation)
@@ -73,17 +73,17 @@ class BoltzRefund {
         )
         
         // I created this .serliazed().hex thing, maybe it's wrong? But at least it looks similar in style as what I get back from the boltz server:
-        let ourBoltzNonceHex = try! ourBoltzNonce.pubnonce.serialized().hex
+        let ourBoltzNonceHex = ourBoltzNonce.pubnonce.hexString
         // ourBoltzNonce: 02cdc4c5b143eb3c575772bf9ac9bac76295188d16c5790306afe5bf4805f0709202bc21c2995ce18c1c8434e4ebc244cecf6cc22515859dc6e9f35d9023df0f33b6
         print("ourBoltzNonce: \(ourBoltzNonceHex)")
         
         // The next few lintes will create the raw transaction of our refund, so that we can send the unsigned transaction to the Boltz API
         // and later on calculate the sighash that we AND the boltz API need to sign
-        let prev_txs = ["0100000000010114ee6d11384b493b84221ae947c29ac97d8905358a011ca7b9eb511631ee20500000000000feffffff02e32a22010000000016001404dee8419071b93a03164ebf7acde824918891653a960400000000002251209b1b3af865fb9cd5b1eee9a348fa5432d844544595940e2fd9af986eb8c67a8f0247304402205d9a5056a83a87433b11a22e46e440e2de6f2752bdb6aa75250a8802799d12a902200690045e74ce21913023a9023d391a4614879dd98e016d52d4e8061ffc3023a90121033ce211fdd9af1d9b4abb5feef0c902f8e6471fd2d6ed0889b25cc20b18a8703ec2000000"];
-        let txids: [String] = ["2a434ca92a59741d164de40e00dedb51368f292c7ada130712f6171e773990a0"];
+        let prev_txs = ["01000000000102cd2a020100591623e557eb4d19d684ab122daea75a13f3c4995b61b4bdbcb62d0000000000feffffff4bd6aa91b409959e97ab1a2b712ef387e03f5ffc406cd38f5107ef4e9a9b68fa0100000000feffffff0278992a010000000016001472a8ba21fdf7e8fd6a07998160fc9c03828eaf20360f030000000000225120aedf0950ebc9bcf716843b1545fc18078409fa3a8e86aa80963c224554b31b4b02473044022057cd6591b377754c8d84e5a354149f815cb7faa6cff3c981adc033679a80aed902204d6191beda76122d8ae5a316de2a36d4820751d989273aac816dfe80f63881210121038a3f4f936b72abeee2e24c89985ab33e62fd81f784cfcc77edb5312a09aba58a024730440220604c5937be664d2b4cefd8ccc79f00db4bffb7412e5167f345c78f6da4f908ac02206dc37e482d1acaa11513a08d9d3afe983a0cafa511f4ea9e534345fc4cd9345a0121038a3f4f936b72abeee2e24c89985ab33e62fd81f784cfcc77edb5312a09aba58ad0000000"];
+        let txids: [String] = ["2927c5ba00bbc5f0c01172bc1f4611215031bad392d0108b0c425ec69c0c23ee"];
         let input_indexs: [UInt32] = [1];
-        let addresses: [String]  = ["bcrt1qr8sqyhmq0ulgumt28znlnz5dy6wkl564r65swh"];
-        let amounts: [UInt64] = [150_000];
+        let addresses: [String]  = ["bcrt1q86yrllp6mzcdxpvgm8rap7cwwpfnrrgvujsqd0"];
+        let amounts: [UInt64] = [199_502];
         
         let base_tx = generateRawTx(prev_txs: prev_txs, txids: txids, input_indexs:input_indexs, addresses:addresses, amounts: amounts);
         
@@ -137,7 +137,7 @@ class BoltzRefund {
                 // ourBoltzPartialSignature: PartialSignature(dataRepresentation: 36 bytes, session: 133 bytes)
                 
                 let boltzAPIPartialSignature = try P256K.Schnorr.PartialSignature(
-                    hexString: partialSignature,
+                    dataRepresentation: Data(partialSignature.bytes),
                     session: ourBoltzPartialSignature.session
                 )
                 
