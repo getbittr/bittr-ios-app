1a62edf68ec6cfa7ff4a4fcbc885faabc8f293e3
Tom Melters <tommelters@gmail.com>
2026-03-30 20:12:06 +0800

    0.1.165

--- bittr/Language.swift
+++ bittr/Language.swift
@@ -510,7 +510,7 @@ class Language: NSObject {
             "alertlightningfees2": "The fees for this instant payment depend on the length of the route between your and the recipient's lightning connections - and the fees charged by intermediaries on that route.",
             "transactiontype": "Transaction type",
             "transactiontype2": "<b>Regular</b>\nGet an address, to receive regular on-chain transactions.\n\n<b>Instant</b>\nCreate an invoice, to receive instant payments into your lightning connection.\n\n<b>Both</b>\nCreate a Bitcoin QR, which contains an on-chain address as well as a lightning invoice.\n\n<b>URL</b>\nIf available, receive instant payments through your lightning URL.",
-            "transactiontype3": "<b>Regular</b>\nSend regular on-chain transactions to an address.\n\n<b>Instant</b>\nEnter an invoice or lightning URL, to make instant bitcoin lightning payments.",
+            "transactiontype3": "<b>Regular</b>\nEnter a bitcoin address, to send regular on-chain transactions.\n\n<b>Instant</b>\nEnter an invoice or lightning URL, to make instant bitcoin lightning payments.",
             "maximumonchain": "Your bittr wallet consists of a bitcoin wallet (for regular payments) and a bitcoin lightning connection (for instant payments).\n\nThe maximum amount you can send for regular payments, is your full regular balance minus the minimum required transaction fees."
             
         ]
